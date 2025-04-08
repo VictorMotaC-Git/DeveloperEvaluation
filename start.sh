a569920b950987ad1f018fbb001a8f8506cfe6d3
Victor Mota Conceição <victor.mota@pixeon.com>
2025-04-08 15:12:26 -0300

    Criando ApiGateway

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+NETWORK_NAME=ambev-shared-network
+
+echo "🔍 Verificando se a rede '$NETWORK_NAME' existe..."
+
+if ! docker network ls | grep -q "$NETWORK_NAME"; then
+  echo "🔧 Rede não encontrada. Criando '$NETWORK_NAME'..."
+  docker network create "$NETWORK_NAME"
+else
+  echo "✅ Rede '$NETWORK_NAME' já existe."
+fi
+
+echo "🚀 Iniciando containers com Docker Compose..."
+docker-compose -f docker-compose.yml up -d --build
+
+echo "🎉 Containers iniciados. Use 'docker ps' para verificar o status."
